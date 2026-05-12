8c9ce7ada2c946701dae245acc417b7545054ca6
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-05-12 14:19:08 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,12 +1,24 @@
 const express = require ('express');
 const playlistsController = require ('./controller/playlists-controller');
 
-const playlistsRouter = express.Router ();
+const playlistsRouter = express.Router()
 
-playlistsRouter.get ('/', playlistsController.index);
-playlistsRouter.get ('/:id', playlistsController.show);
-playlistsRouter.post ('/', playlistsController.save);
-playlistsRouter.put ('/:id', playlistsController.update);
-playlistsRouter.delete ('/:id', playlistsController.delete);
+// PLAYLISTS
+playlistsRouter.get('/', playlistsController.index)
+playlistsRouter.get('/:id', playlistsController.show)
+playlistsRouter.post('/', playlistsController.save)
+playlistsRouter.put('/:id', playlistsController.update)
+playlistsRouter.delete('/:id', playlistsController.delete)
 
-module.exports = playlistsRouter;
+// MUSICS
+playlistsRouter.post(
+  '/:playlistId/musics',
+  playlistsController.addMusic
+)
+
+playlistsRouter.delete(
+  '/:playlistId/musics/:musicId',
+  playlistsController.removeMusic
+)
+
+module.exports = playlistsRouter
